e1ef1c05f0496fe38db8148945c55b1d02d15fd5
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 07:47:15 +0530

    yh

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
 
 
 
 
 
 const PostList = () => {
+
+    const [posts, setPosts] = useState({});
+
+
+    const fetchPosts = async () => {
+        const { data } = await axios.get('http://localhost:5000/posts');
+        console.log(data);
+        setPosts(data);
+    }
+
+
+
     return (
         <div>PostList</div>
     )
