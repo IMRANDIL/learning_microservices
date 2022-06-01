2cc718f662348063292a7846cd7758f561f25623
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 19:28:38 +0530

    yh

--- query/controller/query.js
+++ query/controller/query.js
@@ -1,8 +1,22 @@
 
-
+const posts = {}
 
 exports.postEventsController = (req, res) => {
+    //extract data from the req.body..
+    const { type, data } = req.body;
+    if (type === 'PostCreated') {
+        const { id, title } = data;
+        posts[id] = { id, title, comments: [] }
+    }
 
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data;
+
+        const post = posts[postId];
+        post.comments.push({ id, content });
+    }
+
+    res.send({});
 }
 
 
