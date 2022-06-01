bbbbaf81483813bb2eab23caa485069976ef37fa
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 17:56:41 +0530

    yh

---
+++ query/server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+
+
+
+const PORT = process.env.PORT || 8009;
+
+app.listen(PORT, () => {
+    console.log(`Query service is running on port: ${PORT} 😃`);
+})
