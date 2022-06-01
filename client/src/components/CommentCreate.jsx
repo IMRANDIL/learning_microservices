70e5a1403f8a204eb363a47840dfd42eb6f2b232
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 09:34:51 +0530

    yh

--- client/src/components/CommentCreate.jsx
+++ client/src/components/CommentCreate.jsx
@@ -6,9 +6,33 @@ import axios from 'axios'
 
 
 
-const CommentCreate = () => {
+const CommentCreate = ({ postId }) => {
+    const [content, setContent] = useState('');
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setContent('');
+    }
+
+
+    const handleInput = (e) => {
+        setContent(e.target.value)
+    }
+
+
+
     return (
-        <div>CommentCreate</div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="New Comment">New Comment</label>
+                    <input type="text" className='form-control' value={content} onChange={handleInput} />
+                </div>
+                <button className='btn btn-primary' type='submit'>Submit</button>
+            </form>
+        </div>
     )
 }
 
