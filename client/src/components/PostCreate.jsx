c30c4197c15544f658db2c11d7fa2ef95225bb78
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 07:20:26 +0530

    yh

--- client/src/components/PostCreate.jsx
+++ client/src/components/PostCreate.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios';
+
+
+
+
 
 const PostCreate = () => {
+    const [title, setTitle] = useState('');
+
+
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
+
+
+    const handleInput = () => {
+
+    }
+
+
+
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="Title">Title:</label>
-                    <input type="text" className='form-control' />
+                    <input type="text" className='form-control' value={title} onChange={handleInput} />
                 </div>
                 <button className='btn btn-primary' style={{ marginTop: '15px' }}>Submit</button>
             </form>
