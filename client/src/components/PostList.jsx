4a83368f6009fcee5172b2ff58cfe1103fbc6034
IMRANDIL <aliimranadil2@gmail.com>
2022-06-01 07:56:36 +0530

    yh

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -26,11 +26,23 @@ const PostList = () => {
 
 
 
-    }, [])
+    }, []);
+
+
+    const renderedPosts = Object.values(posts).map((post) => {
+        return <div className='card' key={post.id} style={{ width: '30%', marginBottom: '20px' }}>
+            <div className="card-body">
+                <h3>{post.title}</h3>
+            </div>
+        </div>
+    });
+    console.log(renderedPosts);
 
 
     return (
-        <div>PostList</div>
+        <div className='d-flex flex-row flex-wrap justify-content-between'>
+            {renderedPosts}
+        </div>
     )
 }
 
