da9792fc25a1f1960963eaf380eedcf8c88a884b
IMRANDIL <aliimranadil2@gmail.com>
2022-06-05 17:40:49 +0530

    yh

--- comments/controller/comments.js
+++ comments/controller/comments.js
@@ -55,7 +55,32 @@ exports.createComments = async (req, res) => {
 
 
 
-exports.eventController = (req, res) => {
+exports.eventController = async (req, res) => {
     console.log(`received event ${req.body.type}`);
+    const { type, data } = req.body;
+
+    if (type === 'CommentModerated') {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find((comment) => {
+            return comment.id === id;
+        });
+
+        comment.status = status;
+
+        await axios.post('http://localhost:8005/events', {
+            type: 'CommentUpdated',
+            data: {
+                id,
+                postId,
+                content,
+                status
+
+
+
+            }
+        })
+    }
     res.send({})
 }
