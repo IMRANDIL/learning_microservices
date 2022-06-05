e75393119f7ca05164ead7496bd73214dfbd973d
IMRANDIL <aliimranadil2@gmail.com>
2022-06-05 19:29:35 +0530

    yh

--- event_bus/router/events.js
+++ event_bus/router/events.js
@@ -3,8 +3,22 @@ const router = require('express').Router();
 const axios = require('axios');
 
 
+
+const events = [];
+
+
+router.get('/events', (req, res) => {
+    res.send(events);
+})
+
+
+
+
+
+
 router.post('/events', (req, res) => {
     const event = req.body;
+    events.push(event)
     try {
         axios.post('http://localhost:5000/events', event);
         axios.post('http://localhost:7000/events', event);
