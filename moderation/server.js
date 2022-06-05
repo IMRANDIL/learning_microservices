2cf7bd38a2f30dbab36f68d80542fa4963c4c490
IMRANDIL <aliimranadil2@gmail.com>
2022-06-05 17:25:14 +0530

    yh

--- moderation/server.js
+++ moderation/server.js
@@ -13,7 +13,21 @@ app.use(express.urlencoded({ extended: false }));
 
 
 
-app.post('/events', (req, res) => {
+app.post('/events', async (req, res) => {
+    const { type, data } = req.body;
+
+    if (type === 'CommentCreated') {
+        const status = data.content.includes('shit') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:8005/events', {
+            type: 'CommentModerated',
+            data: {
+                id: data.id,
+                content: data.content,
+                postId: data.postId,
+                status: status
+            }
+        })
+    }
 
 })
 
